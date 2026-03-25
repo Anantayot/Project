1039938ec3214cffd3c244bc9c4a6169fb23c6e8
anantayot <67010974018@msu.ac.th>
2026-03-26 00:54:17 +0700

    ๅ

--- admin/product/product_edit.php
+++ admin/product/product_edit.php
@@ -159,12 +159,6 @@ ob_start();
   }
 </style>
 
-<div class="d-flex justify-content-start mb-4">
-  <a href="products.php" class="btn btn-outline-light btn-sm rounded-pill px-4 py-2 shadow-sm transition-all hover-scale">
-    <i class="bi bi-arrow-left me-1"></i> ย้อนกลับไปหน้าจัดการสินค้า
-  </a>
-</div>
-
 <div class="row justify-content-center">
   <div class="col-lg-8 col-xl-7">
     
