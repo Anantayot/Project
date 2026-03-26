f73fe69ae59924f7ae13784980b72465ed40e1c7
anantayot <67010974018@msu.ac.th>
2026-03-26 13:02:42 +0700

    1

--- admin/index.php
+++ admin/index.php
@@ -11,7 +11,7 @@ if (isset($_SESSION['last_activity'])) {
   $time_inactive = time() - $_SESSION['last_activity'];
   if ($time_inactive >= $timeout_duration) {
     session_unset(); session_destroy();
-    header("Location: ../login.php?timeout=1"); exit;
+    header("Location: /login.php?timeout=1"); exit;
   }
 }
 $_SESSION['last_activity'] = time();
